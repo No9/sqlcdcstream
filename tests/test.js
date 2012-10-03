b9ac95537646c82e983aa5eee438cb5e4be2dd0f
No9 <mrcowsy@gmail.com>
2012-10-03 23:49:03 +0100

    require fix

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,7 @@
 var http = require('http')
 var assert = require('assert')
-var mystream = require('./index.js')
+var mystream = require('../index.js')
 var testcount = 0
 
 var stm = mystream.changes("Driver={SQL Server Native Client 11.0};Server=(local);Database=nodejstest;Trusted_Connection={Yes}", "dbo", "MOVIES", 1000)
 stm.pipe(process.stdout);
-/*spec(stm)
-  .readable()
-  .pausable({strict: true}) //strict is optional.
-  .validateOnExit()
-*/
-  
-
-
-
-//assert.areequal(testcount, 1);
-/*
-var server = http.createServer(function (req, res) {
-    var stream = mystream.changes("MOVIES", 1000);
-    stream.on('error', function (err) {
-        res.statusCode = 500;
-        res.end(String(err));
-    });
-    stream.pipe(res);
-});
-server.listen(8000);*/
